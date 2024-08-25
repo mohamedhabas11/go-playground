3712ad5e6ff788210fc9ad4702deef5f2fff3f18
Mohamed habas <habas.mohamed@gmail.com>
2024-08-25 22:24:50 +0200

    add division func

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,11 +6,11 @@ import (
 	"github.com/mohamedhabas11/go-playground/pkg/math_ops"
 )
 
-
 func main() {
-    fmt.Println("Hello, World!")
-	
+	fmt.Println("Hello, World!")
+
 	// use the imported thing
 	result := math_ops.Add(2, 3)
 	fmt.Printf("2 + 3 = %d\n", result)
+
 }
--- pkg/math_ops/math_ops.go
+++ pkg/math_ops/math_ops.go
@@ -1,10 +1,23 @@
 // Simple Package for basic math opertaions
 package math_ops
 
-func Add( a,b int ) int {
-	return a + b 
+import "errors"
+
+func Add(a, b int) int {
+	return a + b
 }
 
-func Substract( a,b int ) int {
+func Substract(a, b int) int {
 	return a - b
+}
+
+func Multiply(a, b int) int {
+	return a * b
+}
+
+func Divide(a, b int) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("division by zero is not allowed")
+	}
+	return float64(a) / float64(b), nil
 }
