c25607955de43ead48b1ebd6e634c5c5ec779e28
Zawi <ichigo_2002z@hotmail.com>
2024-08-25 22:30:13 +0200

    test

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -13,4 +13,12 @@ func main() {
 	// use the imported thing
 	result := math_ops.Add(2, 3)
 	fmt.Printf("2 + 3 = %d\n", result)
+
+	float_result := math_ops.Multiply(2, 3)
+	fmt.Printf("2 * 3 = %f\n", float_result)
+
+	result = math_ops.Divide(2, 3)
+	fmt.Printf("2 / 3 = %d\n", result)
+
+	fmt.Print(result.(type))
 }
--- pkg/math_ops/math_ops.go
+++ pkg/math_ops/math_ops.go
@@ -1,10 +1,30 @@
 // Simple Package for basic math opertaions
 package math_ops
 
+// Add two numbers
 func Add( a,b int ) int {
 	return a + b 
 }
 
+// Substract two numbers
 func Substract( a,b int ) int {
 	return a - b
+}
+
+// Multiply two numbers
+func Multiply(a, b interface{}) interface{} {
+	switch a.(type) {
+	case int:
+		return a.(int) * b.(int)
+	default:
+		return 0
+	}
+}
+
+
+func Divide( a,b int ) int {
+	if b == 0 {
+		return 0
+	}
+	return a / b
 }
