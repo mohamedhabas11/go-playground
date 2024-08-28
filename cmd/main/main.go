d65c215b27f250873c65ddbcbdd5b5147c6aa816
Mohamed habas <habas.mohamed@gmail.com>
2024-08-29 00:15:35 +0200

    fix internal package export

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	// built-in packages go here
 	"fmt"
 	// selfmade packages go here
-	"github.com/mohamedhabas11/go-playground/pkg/math_ops"
 	"github.com/mohamedhabas11/go-playground/internal/string_utils"
+	// Global packages go here
+	"github.com/mohamedhabas11/go-playground/pkg/math_ops"
 )
 
 func main() {
-	string_utils.greetTheMan("Mohamed")
 	fmt.Println("Hello, World!")
 
-	// use the imported thing
+	// use the imported internal package
+	name := "Mohamed"
+	greet := string_utils.GreetTheMan(name)
+	fmt.Println(greet)
+
+	// use the imported global package
 	result := math_ops.Add(2, 3)
 	fmt.Printf("2 + 3 = %d\n", result)
 
