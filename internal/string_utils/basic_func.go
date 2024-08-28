fd74869968b26b9aab56a65244675706a8bccdb7
Mohamed habas <habas.mohamed@gmail.com>
2024-08-29 00:15:24 +0200

    fix internal package declaration, return type string

--- internal/string_utils/basic_func.go
+++ internal/string_utils/basic_func.go
@@ -1,20 +1,26 @@
 // simple tricky string function
+// Notes:
+// 1. The package name should be the same as the folder name
+// 2. The package name should be the same as the last part of the import path
+// 3. Use Capitalized function names to export them to other packages like 'main'
 
-package basic_func
+package string_utils
 
 import (
 	"fmt"
 	"strings"
 )
 
-func greetTheMan(name string) string {
-	fmt.Println("Hello, " + name)
+// impelement GreetTheMan with printf
+func GreetTheMan(name string) string {
+	// return formatted string
+	return fmt.Sprintf("Hello, %s!", name)
 }
 
-func subTesting() {
-    str := "Say, What?"
-    substring := "Code & Go"
+func SubTesting() {
+	str := "Say, What?"
+	substring := "Code & Go"
 
-    contains := strings.Contains(str, substring)
-    fmt.Println(contains)
+	contains := strings.Contains(str, substring)
+	fmt.Println(contains)
 }
