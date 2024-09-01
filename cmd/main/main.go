17ecf15ddcecc35757173a0f5ec8a8c724102278
Mohamed habas <habas.mohamed@gmail.com>
2024-09-01 23:03:51 +0200

    playing with dictionaries

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,7 +4,9 @@ import (
 	// built-in packages go here
 	"fmt"
 	// selfmade packages go here
+	"github.com/mohamedhabas11/go-playground/internal/dict_utils"
 	"github.com/mohamedhabas11/go-playground/internal/string_utils"
+
 	// Global packages go here
 	"github.com/mohamedhabas11/go-playground/pkg/math_ops"
 )
@@ -29,5 +31,33 @@ func main() {
 
 	result = math_ops.Divide(2, 2, 2)
 	fmt.Printf("2 / 2 / 2 = %f\n", result)
+
+	// use the imported internal package
+	test_dict := map[string]string{
+		"key1": "value1",
+		"key2": "value2",
+		"key3": "value3",
+	}
+
+	// prints [key2 key3 key1] out of order ?
+	fmt.Println(dict_utils.GetKeys(test_dict))
+	// This is because the map is unordered
+	// To get the keys in order, you can sort them
+	// before returning them
+
+	// prints [key1 key2 key3] in order
+	fmt.Println(dict_utils.GetKeysInOrder(test_dict))
+
+	// prints map[key1:value1 key2:value2 key3:value3] in order
+	fmt.Println(dict_utils.SortDict(test_dict))
+
+	// combine two dictionaries
+	test_dict2 := map[string]string{
+		"key4": "value4",
+		"key5": "value5",
+		"key6": "value6",
+	}
+
+	fmt.Println(dict_utils.CombineDicts(test_dict, test_dict2))
 
 }
--- internal/dict_utils/dict_parser.go
+++ internal/dict_utils/dict_parser.go
@@ -1,2 +1,48 @@
 package dict_utils
 
+import "sort"
+
+// Function to return dictionary keys
+func GetKeys(dict map[string]string) []string {
+	keys := make([]string, 0, len(dict))
+	for k := range dict {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+// Function to return dictionary keys in order
+func GetKeysInOrder(dict map[string]string) []string {
+	keys := make([]string, 0, len(dict))
+	for k := range dict {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+// Function to return sorted dictionary object
+func SortDict(dict map[string]string) map[string]string {
+	keys := make([]string, 0, len(dict))
+	for k := range dict {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	sorted_dict := make(map[string]string)
+	for _, k := range keys {
+		sorted_dict[k] = dict[k]
+	}
+	return sorted_dict
+}
+
+// Function that combines two dictionaries
+func CombineDicts(dict1 map[string]string, dict2 map[string]string) map[string]string {
+	combined_dict := make(map[string]string)
+	for k, v := range dict1 {
+		combined_dict[k] = v
+	}
+	for k, v := range dict2 {
+		combined_dict[k] = v
+	}
+	return combined_dict
+}
